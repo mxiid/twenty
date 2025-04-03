mxiid/twenty

commit e6422ad08a048a2004d2a674f91d69922c229392

Only show a workspace member their own email blocklist (#11325)
Fixes https://github.com/twentyhq/twenty/issues/11320

packages/twenty-front/src/modules/settings/accounts/components/SettingsAccountsBlocklistSection.tsx
@@ -16,8 +16,17 @@ export const SettingsAccountsBlocklistSection = () => {
 
   const currentWorkspaceMember = useRecoilValue(currentWorkspaceMemberState);
 
+  if (!currentWorkspaceMember) {
+    throw new Error('No workspace member found.');
+  }
+
   const { records: blocklist } = useFindManyRecords<BlocklistItem>({
     objectNameSingular: CoreObjectNameSingular.Blocklist,
+    filter: {
+      workspaceMemberId: {
+        in: [currentWorkspaceMember.id],
+      },
+    },
   });
 
   const { createOneRecord: createBlocklistItem } =

commit 2468460da7ac58f4ca7bfb1630cae6c1f1c0462a

Hot fix for currentWorkspaceMember not found error on blocklist page (#11367)
Fixes
https://twenty-v7.sentry.io/issues/6504066204/?project=4507072563183616
Introduced by https://github.com/twentyhq/twenty/pull/11325

Called it a hotfix as not sure if missing currentWorkspaceMember is an
expected state - maybe it is expected to be temporarily missing ?

packages/twenty-front/src/modules/settings/accounts/components/SettingsAccountsBlocklistSection.tsx
@@ -10,23 +10,23 @@ import { useFindManyRecords } from '@/object-record/hooks/useFindManyRecords';
 import { SettingsAccountsBlocklistInput } from '@/settings/accounts/components/SettingsAccountsBlocklistInput';
 import { SettingsAccountsBlocklistTable } from '@/settings/accounts/components/SettingsAccountsBlocklistTable';
 import { useLingui } from '@lingui/react/macro';
+import { isDefined } from 'twenty-shared/utils';
 
 export const SettingsAccountsBlocklistSection = () => {
   const { t } = useLingui();
 
   const currentWorkspaceMember = useRecoilValue(currentWorkspaceMemberState);
 
-  if (!currentWorkspaceMember) {
-    throw new Error('No workspace member found.');
-  }
+  const currentWorkspaceMemberId = currentWorkspaceMember?.id ?? '';
 
   const { records: blocklist } = useFindManyRecords<BlocklistItem>({
     objectNameSingular: CoreObjectNameSingular.Blocklist,
     filter: {
       workspaceMemberId: {
-        in: [currentWorkspaceMember.id],
+        in: [currentWorkspaceMemberId],
       },
     },
+    skip: !isDefined(currentWorkspaceMember),
   });
 
   const { createOneRecord: createBlocklistItem } =